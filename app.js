ad036e08e411d6e1b077a927280190230c01b744
Khushboo Priya <khushboopriyaiiitnr@gmail.com>
2021-03-06 20:52:21 +0530

    encoder till tutorial

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const router = express.Router();
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+// the above 2 commands are for post requests, its gives the response in json and string format respectively.
 
+// app.use('/api', api);
+
+app.use(express.static('public')) // to use static files like index.html
 
 // app.use('urls',urls)
-app.get('/', (req, res) => {
-  res.send('First Express Message');
-});
+// app.get('/', (req, res) => {
+//   res.send('First Express Message');
+// });
 
 app.listen(port, () => {
   console.log(`Server listening at port: ${port}`)
---
+++ urls.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const shortid = require('shortid')
+
+const router = express.Router();
+
+const urls = {}
+
+router.get('/',(req,res)=>{
+    // To get the list of all urls
+    const urlList = [];
+    Object.keys(urls).forEach(urlId =>{
+        urlList.push({id:urlId,long_url:long_url })
+    });
+    res.send(urlList);
+});
+
+router.post('/',(req,res)=>{
+    // To post a long url
+    const long_url = req.body.long_url;
+    const id = shortid.generate();
+    urls[id] = long_url;
+    res.status[201].send({id});
+})
+
+router.get('/:id',(req,res) =>{
+    // To get the short url or long url from its id
+    const id = req.params.id;
+    const long_url = urls[id];
+    if (long_url){
+        res.send({
+            id: id,
+            long_url:long_url
+        });
+    }
+    else{
+        res.status(404).send({"error": "Invalid  short url id"});
+    }
+});
+
+module.exports = router;
